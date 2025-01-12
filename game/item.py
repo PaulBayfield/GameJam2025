9e61ba823cdf09c746916d67c172d59d88199ee6
ping0010 <justin.pingard@etudiant.univ-reims.fr>
2025-01-12 22:38:25 +0100

    ajout classe et init Item

--- game/item.py
+++ game/item.py
@@ -1,1 +1,24 @@
+import pygame
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from game.game import Game
+
+
+class Item:
+    """
+    gère les Items qui apparaissent aléatoirement
+    """
+
+    def __init__(self, game: "Game"):
+        """
+        Constructeur de la classe
+
+        :param game: Le jeu
+        :type game: Game
+        """
+        self.game = game
+        self.item_image = pygame.transform.scale(
+            pygame.image.load("assets/chalencon.png"), (20, 20)
+        )
+        self.item_spawn = False
