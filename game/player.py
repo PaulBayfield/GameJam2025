d182ef59736495fb9dfb225263a8659141a365a8
Paul Bayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-01-12 22:23:31 +0100

    feat: Minor changes (#16)

--- game/player.py
+++ game/player.py
@@ -2,7 +2,7 @@ from .dataclasses.player import PlayerData
 
 
 class Player(PlayerData):
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         super().__init__(name)
 
         self.x = 0
@@ -11,14 +11,14 @@ class Player(PlayerData):
         self.health = 100
         self.stamina = 100
 
-    def move(self):
+    def move(self) -> None:
         print(f"{self.name} is moving")
 
-    def attack(self):
+    def attack(self) -> None:
         print(f"{self.name} is attacking")
 
-    def heal(self):
+    def heal(self) -> None:
         print(f"{self.name} is healing")
 
-    def dash(self):
+    def dash(self) -> None:
         print(f"{self.name} is dashing")
