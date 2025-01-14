eb765a5d9ff1f102a9a27c5995bb5c9c712f85c8
PaulBayfield <137620008+PaulBayfield@users.noreply.github.com>
2025-01-14 22:27:09 +0100

    feat: End screen

--- game/controller.py
+++ game/controller.py
@@ -1,6 +1,7 @@
 import pygame
 
 from pygame.event import Event
+from .enums.game import GameState
 from .enums.keyboard import KeyboardType
 from typing import TYPE_CHECKING
 
@@ -35,41 +36,54 @@ class Controller:
 
         # Traite les touches du clavier
         if event.type == pygame.KEYDOWN:
-            # Traite les touches fléchées
-            if event.key in [
-                pygame.K_LEFT,
-                pygame.K_RIGHT,
-                pygame.K_UP,
-                pygame.K_DOWN,
-            ]:
-                self.game.movement.handle(event.key)
+            if self.game.state == GameState.PLAYING:
+                # Traite les touches fléchées
+                if event.key in [
+                    pygame.K_LEFT,
+                    pygame.K_RIGHT,
+                    pygame.K_UP,
+                    pygame.K_DOWN,
+                ]:
+                    self.game.movement.handle(event.key)
 
-            # Traite les touches ZQSD
-            if (
-                self.game.settings.KEYBOARD_TYPE == KeyboardType.ZQSD
-                and event.key
-                in [
-                    pygame.K_q,
-                    pygame.K_d,
-                    pygame.K_z,
-                    pygame.K_s,
-                ]
-            ):
-                self.game.movement.handle(event.key)
+                # Traite les touches ZQSD
+                if (
+                    self.game.settings.KEYBOARD_TYPE == KeyboardType.ZQSD
+                    and event.key
+                    in [
+                        pygame.K_q,
+                        pygame.K_d,
+                        pygame.K_z,
+                        pygame.K_s,
+                    ]
+                ):
+                    self.game.movement.handle(event.key)
 
-            # Traite les touches WASD
-            if (
-                self.game.settings.KEYBOARD_TYPE == KeyboardType.WASD
-                and event.key
-                in [
-                    pygame.K_a,
-                    pygame.K_d,
-                    pygame.K_w,
-                    pygame.K_s,
-                ]
-            ):
-                self.game.movement.handle(event.key)
+                # Traite les touches WASD
+                if (
+                    self.game.settings.KEYBOARD_TYPE == KeyboardType.WASD
+                    and event.key
+                    in [
+                        pygame.K_a,
+                        pygame.K_d,
+                        pygame.K_w,
+                        pygame.K_s,
+                    ]
+                ):
+                    self.game.movement.handle(event.key)
 
-            # Traite la touche ESPACE
-            if event.key == pygame.K_SPACE:
-                self.game.movement.dash()
+                # Traite la touche ESPACE
+                if event.key == pygame.K_SPACE:
+                    self.game.movement.dash()
+
+            # Traite la touche P
+            if event.key == pygame.K_p:
+                if self.game.paused:
+                    self.game.paused = False
+                    self.game.state = GameState.PLAYING
+                else:
+                    self.game.state = GameState.PAUSED
+
+            # Traite la touche ECHAP
+            if event.key == pygame.K_ESCAPE:
+                self.game.state = GameState.END
