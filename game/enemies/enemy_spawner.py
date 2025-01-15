1a38944ce61941b8e6ce1fb3870f2d4bc108707c
Lucas Debeve <lucas.debeve2003@gmail.com>
2025-01-15 20:01:17 +0100

    feat: ennemy spawn

--- game/enemies/enemy_spawner.py
+++ game/enemies/enemy_spawner.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 from game.enemies.knight import Knight
+from game.enemies.pirate import Pirate
+from game.enums.direction import Direction
 
 
 class EnemySpawner:
@@ -8,7 +10,7 @@ class EnemySpawner:
     Classe pour gérer la génération d'ennemis en vagues.
     """
 
-    def __init__(self, game, wave_interval=3000, max_enemies_per_wave=10):
+    def __init__(self, game, wave_interval=3000, max_enemies_per_wave=9):
         """
         Initialise le générateur d'ennemis.
         :param game: Instance du jeu.
@@ -31,25 +33,26 @@ class EnemySpawner:
             if side == "top":
                 x = random.randint(1, self.game.settings.WINDOW_WIDTH - 1)
                 y = 0
-                direction = (0, 1)
+                direction = Direction.DOWN
             elif side == "bottom":
                 x = random.randint(1, self.game.settings.WINDOW_WIDTH - 1)
                 y = self.game.settings.WINDOW_HEIGHT
-                direction = (0, -1)
+                direction = Direction.UP
             elif side == "left":
                 x = 0
                 y = random.randint(1, self.game.settings.WINDOW_HEIGHT - 1)
-                direction = (1, 0)
+                direction = Direction.RIGHT
             elif side == "right":
                 x = self.game.settings.WINDOW_WIDTH
                 y = random.randint(1, self.game.settings.WINDOW_HEIGHT - 1)
-                direction = (-1, 0)
+                direction = Direction.LEFT
 
-            speed = random.uniform(1, 3)
             if random.random() < 0.5:
+                speed = random.uniform(1, 3)
                 enemy = Knight(self.game, x, y, speed, direction)
             else:
-                enemy = Knight(self.game, x, y, speed, direction)
+                speed = random.uniform(2, 4)
+                enemy = Pirate(self.game, x, y, speed, direction)
             self.enemies.add(enemy)
 
     def update(self):
--- game/enemies/pirate.py
+++ game/enemies/pirate.py
@@ -1,4 +1,6 @@
+from game.enums.direction import Direction
 from .enemy import Enemy
+import random
 
 
 class Pirate(Enemy):
@@ -17,3 +19,16 @@ class Pirate(Enemy):
         :param direction: Direction initiale (vecteur [dx, dy]).
         """
         super().__init__(game, x, y, speed, direction, "pirate")
+        self.can_change_direction = False
+
+    def variant(self):
+        """
+        Changer la direction aléatoirement
+        """
+        # Wait 3 seconds before changing direction
+        if self.can_change_direction:
+            self.can_change_direction = False
+            self.direction = random.choice(list(Direction))
+            self.current_sprite_index = 0
+        else:
+            self.can_change_direction = True
